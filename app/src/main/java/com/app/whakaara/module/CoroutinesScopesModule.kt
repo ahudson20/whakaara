9a5fa0fb3d1f17198338b8394939070c9fe8f0d5
Anaru Hudson <anaru.hudson@hotmail.com>
2024-04-16 10:54:03 +0100

    remove singleton scope

--- app/src/main/java/com/app/whakaara/module/CoroutinesScopesModule.kt
+++ app/src/main/java/com/app/whakaara/module/CoroutinesScopesModule.kt
@@ -7,13 +7,11 @@ import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
-import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 object CoroutinesScopesModule {
 
-    @Singleton
     @Provides
     fun providesCoroutineScope(
         @DefaultDispatcher defaultDispatcher: CoroutineDispatcher

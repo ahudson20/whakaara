bcd89ff611d7402b49ee4b281efb21127128dd1f
Anaru Hudson <anaru.hudson@hotmail.com>
2024-04-24 22:16:01 +0100

    more unit + integration tests

---
+++ app/src/test/java/com/app/whakaara/logic/StopwatchManagerTest.kt
@@ -0,0 +1,217 @@
+package com.app.whakaara.logic
+
+import android.app.Application
+import android.app.NotificationManager
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.core.app.NotificationCompat
+import app.cash.turbine.test
+import com.app.whakaara.data.datastore.PreferencesDataStore
+import com.app.whakaara.state.Lap
+import com.app.whakaara.state.StopwatchState
+import io.mockk.Runs
+import io.mockk.coEvery
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.slot
+import io.mockk.verify
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class StopwatchManagerTest {
+    @Rule
+    @JvmField
+    var rule: TestRule = InstantTaskExecutorRule()
+
+    private val testDispatcher = StandardTestDispatcher()
+
+    private lateinit var stopwatchManagerWrapper: StopwatchManagerWrapper
+    private lateinit var app: Application
+    private lateinit var notificationManager: NotificationManager
+    private lateinit var stopwatchNotificationBuilder: NotificationCompat.Builder
+    private lateinit var coroutineScope: CoroutineScope
+    private lateinit var preferencesDatastore: PreferencesDataStore
+    private val managedCoroutineScope = TestScope(testDispatcher)
+    private val id = 400
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(testDispatcher)
+        app = mockk()
+        notificationManager = mockk()
+        stopwatchNotificationBuilder = mockk()
+        coroutineScope = mockk()
+        preferencesDatastore = mockk()
+        stopwatchManagerWrapper = StopwatchManagerWrapper(app, notificationManager, stopwatchNotificationBuilder, managedCoroutineScope, preferencesDatastore)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun `pause stopwatch`() = runTest {
+        // Given
+        val notificationId = slot<Int>()
+        coEvery { notificationManager.cancel(any()) } just Runs
+
+        // When
+        stopwatchManagerWrapper.pauseStopwatch()
+
+        // Then
+        verify(exactly = 1) { notificationManager.cancel(capture(notificationId)) }
+        assertEquals(id, notificationId.captured)
+        stopwatchManagerWrapper.stopwatchState.test {
+            val state = awaitItem()
+            with(state) {
+                assertEquals(false, isActive)
+                assertEquals(true, isPaused)
+            }
+        }
+    }
+
+    @Test
+    fun `reset stopwatch`() = runTest {
+        // Given
+        val notificationId = slot<Int>()
+        coEvery { notificationManager.cancel(any()) } just Runs
+
+        // When
+        stopwatchManagerWrapper.resetStopwatch()
+
+        // Then
+        verify(exactly = 1) { notificationManager.cancel(capture(notificationId)) }
+        assertEquals(id, notificationId.captured)
+        stopwatchManagerWrapper.stopwatchState.test {
+            val state = awaitItem()
+            with(state) {
+                assertEquals(0L, timeMillis)
+                assertEquals(0L, lastTimeStamp)
+                assertEquals("00:00:00:000", formattedTime)
+                assertEquals(false, isActive)
+                assertEquals(true, isStart)
+                assertEquals(false, isPaused)
+                assert(lapList.isEmpty())
+            }
+        }
+    }
+
+    @Test
+    fun `lap stopwatch empty list`() = runTest {
+        // Given
+        val current = 123123L
+        stopwatchManagerWrapper.stopwatchState.value = StopwatchState(
+            timeMillis = current
+        )
+
+        // When
+        stopwatchManagerWrapper.lapStopwatch()
+
+        // Then
+        stopwatchManagerWrapper.stopwatchState.test {
+            val state = awaitItem()
+            with(state) {
+                assert(lapList.isNotEmpty())
+                assertEquals(1, lapList.size)
+                assertEquals(current, lapList.first().diff)
+                assertEquals(current, lapList.first().time)
+            }
+        }
+    }
+
+    @Test
+    fun `lap stopwatch not empty list`() = runTest {
+        // Given
+        val current = 123123L
+        stopwatchManagerWrapper.stopwatchState.value = StopwatchState(
+            timeMillis = current,
+            lapList = mutableListOf(
+                Lap(
+                    time = 123L,
+                    diff = 123L
+                ),
+                Lap(
+                    time = 456L,
+                    diff = 456L
+                )
+            )
+        )
+
+        // When
+        stopwatchManagerWrapper.lapStopwatch()
+
+        // Then
+        stopwatchManagerWrapper.stopwatchState.test {
+            val state = awaitItem()
+            with(state) {
+                assert(lapList.isNotEmpty())
+                assertEquals(3, lapList.size)
+                assertEquals(123123, lapList.last().time)
+                assertEquals(122667, lapList.last().diff)
+            }
+        }
+    }
+
+    @Test
+    fun `recreate stopwatch paused`() = runTest {
+        // Given
+        val pausedState = StopwatchState(
+            isPaused = true,
+            isActive = false,
+            isStart = false,
+            timeMillis = 112233L,
+            formattedTime = "11:22:33:444",
+            lapList = mutableListOf(
+                Lap(
+                    time = 420L,
+                    diff = 420L
+                )
+            )
+        )
+
+        // When
+        stopwatchManagerWrapper.recreateStopwatchPaused(state = pausedState)
+
+        // Then
+        stopwatchManagerWrapper.stopwatchState.test {
+            val state = awaitItem()
+            with(state) {
+                assertEquals(true, isPaused)
+                assertEquals(false, isActive)
+                assertEquals(false, isStart)
+                assertEquals(112233L, timeMillis)
+                assertEquals("11:22:33:444", formattedTime)
+                assertEquals(1, lapList.size)
+                assertEquals(420L, lapList.first().time)
+                assertEquals(420L, lapList.first().diff)
+            }
+        }
+    }
+
+    @Test
+    fun `cancel notification`() = runTest {
+        // Given
+        val notificationId = slot<Int>()
+        coEvery { notificationManager.cancel(any()) } just Runs
+
+        // When
+        stopwatchManagerWrapper.cancelNotification()
+
+        // Then
+        verify(exactly = 1) { notificationManager.cancel(capture(notificationId)) }
+        assertEquals(id, notificationId.captured)
+    }
+}
--- app/src/test/java/com/app/whakaara/logic/TimerManagerWrapperTest.kt
+++ app/src/test/java/com/app/whakaara/logic/TimerManagerWrapperTest.kt
@@ -7,11 +7,17 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.core.app.NotificationCompat
 import app.cash.turbine.test
 import com.app.whakaara.data.datastore.PreferencesDataStore
+import com.app.whakaara.utils.DateUtils
+import io.mockk.Runs
+import io.mockk.coEvery
+import io.mockk.just
 import io.mockk.mockk
-import kotlinx.coroutines.CoroutineScope
+import io.mockk.slot
+import io.mockk.verify
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
@@ -21,6 +27,7 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestRule
+import java.util.concurrent.TimeUnit
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class TimerManagerWrapperTest {
@@ -37,7 +44,7 @@ class TimerManagerWrapperTest {
     private lateinit var timerNotificationBuilder: NotificationCompat.Builder
     private lateinit var countDownTimerUtil: CountDownTimerUtil
     private lateinit var preferencesDatastore: PreferencesDataStore
-    private lateinit var coroutineScope: CoroutineScope
+    private val managedCoroutineScope = TestScope(testDispatcher)
 
     @Before
     fun setUp() {
@@ -48,9 +55,11 @@ class TimerManagerWrapperTest {
         timerNotificationBuilder = mockk()
         countDownTimerUtil = mockk()
         preferencesDatastore = mockk()
-        coroutineScope = mockk()
 
-        timerManagerWrapper = TimerManagerWrapper(app, alarmManager, notificationManager, timerNotificationBuilder, countDownTimerUtil, preferencesDatastore, coroutineScope)
+        timerManagerWrapper = TimerManagerWrapper(app, alarmManager, notificationManager, timerNotificationBuilder, countDownTimerUtil, preferencesDatastore, managedCoroutineScope)
+
+        coEvery { countDownTimerUtil.countdown(any(), any(), any(), any()) } just Runs
+        coEvery { countDownTimerUtil.cancel() } just Runs
     }
 
     @After
@@ -107,5 +116,72 @@ class TimerManagerWrapperTest {
                 assertEquals("42", inputSeconds)
             }
         }
+    }
+
+    @Test
+    fun `recreate active timer`() = runTest {
+        // Given
+        val millis = 123123L
+
+        // When
+        timerManagerWrapper.recreateActiveTimer(milliseconds = millis)
+
+        // Then
+        verify(exactly = 1) { countDownTimerUtil.cancel() }
+        verify(exactly = 1) { countDownTimerUtil.countdown(any(), any(), any(), any()) }
+        timerManagerWrapper.timerState.test {
+            val state = awaitItem()
+            with(state) {
+                assertEquals(false, isTimerPaused)
+                assertEquals(false, isStart)
+                assertEquals(true, isTimerActive)
+                assertEquals(millis, millisecondsFromTimerInput)
+                assertEquals(TimeUnit.MILLISECONDS.toHours(millis).toString(), inputHours)
+                assertEquals(TimeUnit.MILLISECONDS.toMinutes(millis).toString(), inputMinutes)
+                assertEquals(TimeUnit.MILLISECONDS.toSeconds(millis).toString(), inputSeconds)
+                cancelAndIgnoreRemainingEvents()
+            }
+        }
+    }
+
+    @Test
+    fun `recreate paused timer`() = runTest {
+        // Given
+        val millis = 123123L
+        val millisFormatted = DateUtils.formatTimeForTimer(
+            millis = millis
+        )
+
+        // When
+        timerManagerWrapper.recreatePausedTimer(milliseconds = millis)
+
+        // Then
+        timerManagerWrapper.timerState.test {
+            val state = awaitItem()
+            with(state) {
+                assertEquals(true, isTimerPaused)
+                assertEquals(false, isStart)
+                assertEquals(false, isTimerActive)
+                assertEquals(millis, currentTime)
+                assertEquals(millis, millisecondsFromTimerInput)
+                assertEquals(millisFormatted, time)
+                cancelAndIgnoreRemainingEvents()
+            }
+        }
+    }
+
+    @Test
+    fun `cancel notification`() = runTest {
+        // Given
+        val id = 300
+        val notificationId = slot<Int>()
+        coEvery { notificationManager.cancel(any()) } just Runs
+
+        // When
+        timerManagerWrapper.cancelNotification()
+
+        // Then
+        verify(exactly = 1) { notificationManager.cancel(capture(notificationId)) }
+        assertEquals(id, notificationId.captured)
     }
 }

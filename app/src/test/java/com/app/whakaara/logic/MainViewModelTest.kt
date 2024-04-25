72037e9b7a80fedaf9f274dc7fe0179b877292df
Anaru Hudson <anaru.hudson@hotmail.com>
2024-04-25 19:02:43 +0100

    viewmodel unit tests for alarm related functions

--- app/src/test/java/com/app/whakaara/logic/MainViewModelTest.kt
+++ app/src/test/java/com/app/whakaara/logic/MainViewModelTest.kt
@@ -2,6 +2,7 @@ package com.app.whakaara.logic
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import app.cash.turbine.test
+import com.app.whakaara.MainDispatcherRule
 import com.app.whakaara.data.alarm.Alarm
 import com.app.whakaara.data.alarm.AlarmRepository
 import com.app.whakaara.data.datastore.PreferencesDataStore
@@ -12,17 +13,13 @@ import com.app.whakaara.state.AlarmState
 import com.app.whakaara.state.StopwatchState
 import com.app.whakaara.state.TimerState
 import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.every
 import io.mockk.mockk
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import io.mockk.slot
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.test.StandardTestDispatcher
-import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
-import kotlinx.coroutines.test.setMain
-import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Before
@@ -33,7 +30,6 @@ import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 import java.util.Calendar
 
-@OptIn(ExperimentalCoroutinesApi::class)
 @RunWith(JUnit4::class)
 class MainViewModelTest {
 
@@ -41,37 +37,28 @@ class MainViewModelTest {
     @JvmField
     var rule: TestRule = InstantTaskExecutorRule()
 
-    private val testDispatcher = StandardTestDispatcher()
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
 
     private lateinit var viewModel: MainViewModel
-    private lateinit var repository: AlarmRepository
-    private lateinit var preferencesRepository: PreferencesRepository
+    private var repository: AlarmRepository = mockk(relaxed = true)
+    private var preferencesRepository: PreferencesRepository = mockk(relaxed = true)
+    private var alarmManagerWrapper: AlarmManagerWrapper = mockk(relaxed = true)
+    private var timerManagerWrapper: TimerManagerWrapper = mockk(relaxed = true)
+    private var stopwatchManagerWrapper: StopwatchManagerWrapper = mockk(relaxed = true)
+    private var preferencesDataStore: PreferencesDataStore = mockk(relaxed = true)
     private lateinit var preferences: Preferences
     private lateinit var alarms: List<Alarm>
-    private lateinit var alarmManagerWrapper: AlarmManagerWrapper
-    private lateinit var timerManagerWrapper: TimerManagerWrapper
-    private lateinit var stopwatchManagerWrapper: StopwatchManagerWrapper
     private lateinit var stopwatchState: StopwatchState
     private lateinit var timerState: TimerState
-    private lateinit var preferencesDataStore: PreferencesDataStore
 
     @Before
     fun setUp() {
-        Dispatchers.setMain(testDispatcher)
-        repository = mockk()
-        preferencesRepository = mockk()
-        alarmManagerWrapper = mockk()
-        timerManagerWrapper = mockk()
-        stopwatchManagerWrapper = mockk()
-        preferencesDataStore = mockk()
         stopwatchState = StopwatchState()
         timerState = TimerState()
 
         every { stopwatchManagerWrapper.stopwatchState } returns MutableStateFlow(stopwatchState)
         every { timerManagerWrapper.timerState } returns MutableStateFlow(timerState)
-
-        viewModel = MainViewModel(repository, preferencesRepository, alarmManagerWrapper, timerManagerWrapper, stopwatchManagerWrapper, preferencesDataStore)
-
         alarms = listOf(
             Alarm(
                 date = Calendar.getInstance().apply {
@@ -101,12 +88,21 @@ class MainViewModelTest {
         preferences = Preferences()
 
         coEvery { repository.getAllAlarmsFlow() } returns flowOf(alarms)
-        coEvery { preferencesRepository.getPreferencesFlow() } returns flowOf(preferences)
-    }
+        coEvery { repository.insert(any()) } returns Unit
+        coEvery { repository.delete(any()) } returns Unit
+        coEvery { repository.update(any()) } returns Unit
 
-    @After
-    fun tearDown() {
-        Dispatchers.resetMain()
+        coEvery { preferencesRepository.getPreferencesFlow() } returns flowOf(preferences)
+        coEvery { preferencesRepository.updatePreferences(any()) } returns Unit
+
+        coEvery { alarmManagerWrapper.createAlarm(any(), any(), any(), any(), any(), any(), any()) } returns Unit
+        coEvery { alarmManagerWrapper.stopStartUpdateWidget(any(), any(), any(), any(), any(), any(), any()) } returns Unit
+        coEvery { alarmManagerWrapper.deleteAlarm(any()) } returns Unit
+        coEvery { alarmManagerWrapper.setUpcomingAlarm(any(), any(), any(), any(), any(), any()) } returns Unit
+        coEvery { alarmManagerWrapper.updateWidget() } returns Unit
+        coEvery { alarmManagerWrapper.cancelUpcomingAlarm(any(), any()) } returns Unit
+
+        viewModel = MainViewModel(repository, preferencesRepository, alarmManagerWrapper, timerManagerWrapper, stopwatchManagerWrapper, preferencesDataStore)
     }
 
     @Test
@@ -150,5 +146,247 @@ class MainViewModelTest {
 
             cancelAndIgnoreRemainingEvents()
         }
+    }
+
+    @Test
+    fun `update preferences`() = runTest {
+        // Given
+        val preferences = Preferences(
+            autoSilenceTime = SettingsTime.TEN,
+            snoozeTime = SettingsTime.FIFTEEN
+        )
+        val preferencesSlot = slot<Preferences>()
+
+        // When
+        viewModel.updatePreferences(preferences = preferences)
+
+        // Then
+        coVerify(exactly = 1) { preferencesRepository.updatePreferences(capture(preferencesSlot)) }
+        assertEquals(SettingsTime.TEN, preferencesSlot.captured.autoSilenceTime)
+        assertEquals(SettingsTime.FIFTEEN, preferencesSlot.captured.snoozeTime)
+    }
+
+    @Test
+    fun `create alarm`() = runTest {
+        // Given
+        val alarm = Alarm(
+            date = Calendar.getInstance().apply {
+                set(Calendar.HOUR_OF_DAY, 12)
+                set(Calendar.MINUTE, 34)
+            },
+            subTitle = "10:03PM"
+        )
+        val alarmSlot = slot<Alarm>()
+
+        // When
+        viewModel.create(alarm = alarm)
+
+        // Then
+        coVerify(exactly = 1) { repository.insert(capture(alarmSlot)) }
+        coVerify(exactly = 1) { alarmManagerWrapper.createAlarm(alarm.alarmId.toString(), alarm.date, any(), any(), any(), alarm.repeatDaily, alarm.daysOfWeek) }
+        with(alarmSlot.captured) {
+            assertEquals(alarm.date, date)
+            assertEquals("10:03PM", subTitle)
+        }
+    }
+
+    @Test
+    fun `delete alarm`() = runTest {
+        // Given
+        val alarm = Alarm(
+            date = Calendar.getInstance().apply {
+                set(Calendar.HOUR_OF_DAY, 12)
+                set(Calendar.MINUTE, 34)
+            },
+            subTitle = "10:03PM"
+        )
+        val alarmSlot = slot<Alarm>()
+        val alarmIdSlot = slot<String>()
+
+        // When
+        viewModel.delete(alarm = alarm)
+
+        // Then
+        coVerify(exactly = 1) { repository.delete(capture(alarmSlot)) }
+        coVerify(exactly = 1) { alarmManagerWrapper.deleteAlarm(capture(alarmIdSlot)) }
+        coVerify(exactly = 1) { alarmManagerWrapper.cancelUpcomingAlarm(alarmId = any(), alarmDate = any()) }
+        with(alarmSlot.captured) {
+            assertEquals(alarm.date, date)
+            assertEquals("10:03PM", subTitle)
+        }
+        assertEquals(alarm.alarmId.toString(), alarmIdSlot.captured)
+    }
+
+    @Test
+    fun `disable alarm`() = runTest {
+        // Given
+        val alarm = Alarm(
+            date = Calendar.getInstance().apply {
+                set(Calendar.HOUR_OF_DAY, 12)
+                set(Calendar.MINUTE, 34)
+            },
+            subTitle = "10:03PM"
+        )
+        val alarmSlot = slot<Alarm>()
+        val alarmIdSlot = slot<String>()
+
+        // When
+        viewModel.disable(alarm = alarm)
+
+        // Then
+        coVerify(exactly = 1) { repository.update(capture(alarmSlot)) }
+        coVerify(exactly = 1) { alarmManagerWrapper.deleteAlarm(alarmId = capture(alarmIdSlot)) }
+        coVerify(exactly = 1) { alarmManagerWrapper.cancelUpcomingAlarm(alarmId = any(), alarmDate = any()) }
+        with(alarmSlot.captured) {
+            assertEquals(alarm.date, date)
+            assertEquals("10:03PM", subTitle)
+            assertEquals(false, isEnabled)
+        }
+        assertEquals(alarm.alarmId.toString(), alarmIdSlot.captured)
+    }
+
+    @Test
+    fun `enable alarm`() = runTest {
+        // Given
+        val alarm = Alarm(
+            date = Calendar.getInstance().apply {
+                set(Calendar.HOUR_OF_DAY, 12)
+                set(Calendar.MINUTE, 34)
+            },
+            subTitle = "10:03PM"
+        )
+        val alarmSlot = slot<Alarm>()
+
+        // When
+        viewModel.enable(alarm = alarm)
+
+        // Then
+        coVerify(exactly = 1) { repository.update(capture(alarmSlot)) }
+        coVerify(exactly = 1) { alarmManagerWrapper.stopStartUpdateWidget(alarmId = alarm.alarmId.toString(), date = alarm.date, any(), any(), any(), alarm.repeatDaily, alarm.daysOfWeek) }
+        with(alarmSlot.captured) {
+            assertEquals(alarm.date, date)
+            assertEquals("10:03PM", subTitle)
+            assertEquals(true, isEnabled)
+        }
+    }
+
+    @Test
+    fun `reset alarm`() = runTest {
+        // Given
+        val alarm = Alarm(
+            date = Calendar.getInstance().apply {
+                set(Calendar.HOUR_OF_DAY, 12)
+                set(Calendar.MINUTE, 34)
+            },
+            subTitle = "10:03PM"
+        )
+        val alarmSlot = slot<Alarm>()
+        val alarmIdSlot = slot<String>()
+
+        // When
+        viewModel.reset(alarm = alarm)
+
+        // Then
+        coVerify(exactly = 1) { repository.update(capture(alarmSlot)) }
+        coVerify(exactly = 1) { alarmManagerWrapper.stopStartUpdateWidget(alarmId = capture(alarmIdSlot), date = alarm.date, any(), any(), any(), alarm.repeatDaily, alarm.daysOfWeek) }
+        with(alarmSlot.captured) {
+            assertEquals(alarm.date, date)
+            assertEquals("10:03PM", subTitle)
+            assertEquals(true, isEnabled)
+        }
+        assertEquals(alarm.alarmId.toString(), alarmIdSlot.captured)
+    }
+
+    @Test
+    fun `snooze alarm`() = runTest {
+        // Given
+        val alarm = Alarm(
+            date = Calendar.getInstance().apply {
+                set(Calendar.HOUR_OF_DAY, 12)
+                set(Calendar.MINUTE, 34)
+            },
+            subTitle = "10:03PM"
+        )
+        val alarmIdSlot = slot<String>()
+
+        // When
+        viewModel.snooze(alarm = alarm)
+
+        // Then
+        coVerify(exactly = 1) { alarmManagerWrapper.stopStartUpdateWidget(alarmId = capture(alarmIdSlot), date = any(), any(), any(), any(), alarm.repeatDaily, alarm.daysOfWeek) }
+        assertEquals(alarm.alarmId.toString(), alarmIdSlot.captured)
+    }
+
+    @Test
+    fun `update all alarm subtitles 12 hour format`() = runTest {
+        // Given
+        val alarmSlots = mutableListOf<Alarm>()
+//        alarms.forEach {
+//            assertEquals("14:34 PM", it.subTitle)
+//        }
+
+        // When
+        viewModel.updateAllAlarmSubtitles(format = false)
+
+        // Then
+        coVerify(exactly = 2) { repository.update(capture(alarmSlots)) }
+        coVerify(exactly = 1) { alarmManagerWrapper.updateWidget() }
+        alarmSlots.forEach {
+            assertEquals("2:34 PM", it.subTitle)
+        }
+    }
+
+    @Test
+    fun `update all alarm subtitles 24 hour format`() = runTest {
+        // Given
+        val alarmSlots = mutableListOf<Alarm>()
+        alarms.forEach {
+            assertEquals("14:34 PM", it.subTitle)
+        }
+
+        // When
+        viewModel.updateAllAlarmSubtitles(format = true)
+
+        // Then
+        coVerify(exactly = 2) { repository.update(capture(alarmSlots)) }
+        coVerify(exactly = 1) { alarmManagerWrapper.updateWidget() }
+        alarmSlots.forEach {
+            assertEquals("14:34", it.subTitle)
+        }
+    }
+
+    @Test
+    fun `update current alarms to add upcoming alarm notification`() = runTest {
+        // Given
+        val alarmIdSlots = mutableListOf<String>()
+        val upcomingAlarmNotificationEnabledSlots = mutableListOf<Boolean>()
+
+        // When
+        viewModel.updateCurrentAlarmsToAddOrRemoveUpcomingAlarmNotification(true)
+
+        // Then
+        coVerify(exactly = 2) { alarmManagerWrapper.setUpcomingAlarm(capture(alarmIdSlots), any(), capture(upcomingAlarmNotificationEnabledSlots), any(), any(), any()) }
+        assertEquals(2, alarmIdSlots.size)
+        assertEquals(alarms.first().alarmId.toString(), alarmIdSlots.first())
+        assertEquals(alarms.last().alarmId.toString(), alarmIdSlots.last())
+        assertEquals(2, upcomingAlarmNotificationEnabledSlots.size)
+        upcomingAlarmNotificationEnabledSlots.forEach {
+            assertEquals(true, it)
+        }
+    }
+
+    @Test
+    fun `update current alarms to remove upcoming alarm notification`() = runTest {
+        // Given
+        val alarmIdSlots = mutableListOf<String>()
+
+        // When
+        viewModel.updateCurrentAlarmsToAddOrRemoveUpcomingAlarmNotification(false)
+
+        // Then
+        coVerify(exactly = 2) { alarmManagerWrapper.cancelUpcomingAlarm(capture(alarmIdSlots), any()) }
+        assertEquals(2, alarmIdSlots.size)
+        assertEquals(alarms.first().alarmId.toString(), alarmIdSlots.first())
+        assertEquals(alarms.last().alarmId.toString(), alarmIdSlots.last())
     }
 }

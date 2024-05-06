81830da05680a627d2d6eb5307ef3d8c34f8969a
Anaru Hudson <anaru.hudson@hotmail.com>
2024-05-06 14:24:30 +0100

    ktlint

--- core-test/src/main/java/com/whakaara/test/MainDispatcherRule.kt
+++ core-test/src/main/java/com/whakaara/test/MainDispatcherRule.kt
@@ -11,7 +11,7 @@ import org.junit.runner.Description
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class MainDispatcherRule(
-    private val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
+    private val testDispatcher: TestDispatcher = UnconfinedTestDispatcher(),
 ) : TestWatcher() {
     override fun starting(description: Description) {
         Dispatchers.setMain(testDispatcher)
